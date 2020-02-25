7ba86878083bc1990462dc8e604bff154b132c7b
ainslee <54660705+cdralenko@users.noreply.github.com>
2020-02-24 22:30:48 -0500

    monday update

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    private void Start()
+    {
+        // put weird menu script here if broken
+        SceneManager.UnloadSceneAsync("WinScreen");
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("ConnorScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
